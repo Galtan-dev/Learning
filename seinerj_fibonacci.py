5b1191f7b05a8df13bac3cfc5831f844fc29edf0
Galtan-dev <jakub.seiner@seznam.cz>
2023-09-23 13:27:46 +0200

    second update of assigment

---
+++ seinerj_fibonacci.py
@@ -0,0 +1,27 @@
+"""
+Script for calculation of sum of numbers of fibonachi sequence lower than 5 000 000
+"""
+
+# definition of initial lists and variables
+List_of_fib_nums = [0, 1]
+LAST_FIB_NUM = 0
+TIME = 2
+SUMMARIZATION = 0
+
+# start of cycle which control if fibonacci number is not greater than 5 000 000
+while LAST_FIB_NUM < 5000000:
+    # equation defining fibonacci sequence
+    List_of_fib_nums.append(List_of_fib_nums[TIME - 1] + List_of_fib_nums[TIME - 2])
+    # time update which move cycle towards
+    TIME += 1
+    # save last fib number from list to variable
+    LAST_FIB_NUM = List_of_fib_nums[len(List_of_fib_nums) - 1]
+    # considering if last fibonacci number is even
+    if (LAST_FIB_NUM % 2) == 0:
+        # sum of all fibonacchi numbers till the condition allow continue
+        SUMMARIZATION += LAST_FIB_NUM
+    else:
+        pass
+
+# visualization of sum of even fibonacchi numbers
+print(SUMMARIZATION)
--- seinerj_nasobky.py
+++ seinerj_nasobky.py
@@ -2,19 +2,25 @@
 Script for calculation multiples of three and five lower than 1024
 """
 
-END_OF_INTERVAL = 1024
+END_OF_INTERVAL = 1234
 END_CONDITION = 0
 
 # cycle that control if calculation is in range
 while END_OF_INTERVAL != 0:
     # first condition that collect numbers dividable only by 3 and lower that biggest number
-    if (END_OF_INTERVAL % 3) == 0 and (END_OF_INTERVAL % 5) != 0 and (END_OF_INTERVAL < 1024):
+    if (END_OF_INTERVAL % 3) == 0 and (END_OF_INTERVAL % 5) != 0 and (END_OF_INTERVAL < 1234):
         # variable to which are collected numbers saved
         END_CONDITION += END_OF_INTERVAL
         # counter
         END_OF_INTERVAL -= 1
     # second condition that collect numbers dividable only by 5 and lower that biggest number
-    elif (END_OF_INTERVAL % 5) == 0 and (END_OF_INTERVAL % 3) != 0 and (END_OF_INTERVAL < 1024):
+    elif (END_OF_INTERVAL % 5) == 0 and (END_OF_INTERVAL % 3) != 0 and (END_OF_INTERVAL < 1234):
+        # variable to which are collected numbers saved
+        END_CONDITION += END_OF_INTERVAL
+        # counter
+        END_OF_INTERVAL -= 1
+    # third condition that collect numbers dividable by 5 and 3 and lower that biggest number
+    elif (END_OF_INTERVAL % 5) == 0 and (END_OF_INTERVAL % 3) == 0 and (END_OF_INTERVAL < 1234):
         # variable to which are collected numbers saved
         END_CONDITION += END_OF_INTERVAL
         # counter
